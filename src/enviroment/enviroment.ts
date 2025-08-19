135e27fba87df2f0d754a2c81ee18d6e9286be15
Sudotec <henriquepedro.piola@gmail.com>
2025-08-19 19:53:16 -0300

    novo commit

--- src/enviroment/enviroment.ts
+++ src/enviroment/enviroment.ts
@@ -1,3 +1,20 @@
 export const environment = {
     production: false
 };
+
+export const SpotifyConsfiguration = {
+    clientId: '7e6a8b33c9c24058833a2ad91bcb1b93',
+    authEndpoint: 'https://accounts.spotify.com/authorize',
+    apiTokenEndpoint: 'https://accounts.spotify.com/api/token',
+    redirectUrl: 'http://127.0.0.1:5173/callback',
+    scopes: [
+        'user-read-currently-playing', //Musica que esta tocando
+        'user-read-recently-played', //Musicas que foram tocadas recentemente
+        'user-read-playback-state', //Estado da reproduçao
+        'user-top-read', //Top artistas e musicas
+        'user-modify-playback-state', //Controlar a reproduçao
+        'user-library-read', //Ler biblioteca de musicas
+        'playlist-read-private', //Ler playlists privadas
+        'playlist-read-collaborative' //Ler playlists colaborativas
+    ]
+}
